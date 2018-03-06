3e282725262da75b0945a0bdd4a844590419bd41
Yarden <yardenn1@gmail.com>
2018-03-05 20:53:33 -0500

    Mobile responsive index page

--- public/index.js
+++ public/index.js
@@ -4,4 +4,12 @@ app.controller('controller', function ($scope, $http) {
   $scope.class = function(name) {
     $scope.class_name = name;  
   }
+  $scope.detail_text = "Details";
+  $scope.toggle_show = function() {
+    $("#sidebar").toggle();
+    if($scope.detail_text == "Details")
+      $scope.detail_text = "Less";
+    else
+      $scope.detail_text = "Details";
+  }
 });
