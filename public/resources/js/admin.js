1359d60827d290604bd7ff895a6ee7e28c0bb004
Yarden <yardenn1@gmail.com>
2018-03-28 16:16:55 -0400

    Tweaks to payment and modal forms

--- public/resources/js/admin.js
+++ public/resources/js/admin.js
@@ -5,6 +5,7 @@ app.controller('controller', function ($scope, $http) {
   $scope.instructor_accounts = [{"name": "Sally", "email": "sally@gmail.com", "classes": ["Yoga", "Pilates"], "bio": "I'm Sally and I'm cool", "image": "/resources/img/i2.jpg"}, {"name": "Bill", "email": "bill@gmail.com", "classes": ["Boxing", "Zumba"], "bio": "I'm Bill and I'm cool", "image": "/resources/img/i3.jpg"}];
   $scope.member_accounts = [{"name": "Yarden Ne'eman", "email": "neemay@rpi.edu", "rin": "660000000", "payment_method": "bursar", "classes": ["Kettlebell Kickboxing", "Core Yoga"], "payment_status": "paid"}, {"name": "Yarden Ne'eman", "email": "neemay@rpi.edu", "rin": "660000000", "payment_method": "bursar", "classes": ["Kettlebell Kickboxing", "Core Yoga"], "payment_status": "paid"}];
   $scope.classes = true;
+  $scope.payment = "0";
   $scope.changeTab = function(event, val) {
     $(".nav-link").removeClass("active");
     $(event.target).addClass("active");
