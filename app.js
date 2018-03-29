5e9a1a70365c715b8b38785cc25bc9f1cfca33da
Collin Jones <jonesc11@rpi.edu>
2018-03-29 12:03:46 -0400

    cdj - set up mongo connection and is_admin

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var path = require ('path');
 var express = require ('express');
 var app = express();
+var mongo = require ('mongodb').MongoClient;
+
+var accountsCollection = null;
+var classesCollection = null;
 
 app.use (express.static (__dirname + '/public'));
 
@@ -15,5 +19,40 @@ app.get ('/instructors', function (req, res) {
 app.get ('/admin', function (req, res) {
   res.sendFile (__dirname + "/pages/admin.html");
 });
+
+var mongoUrl = 'mongodb://ec2-34-239-101-4.compute-1.amazonaws.com';
+
+mongo.connect (mongoUrl, function (err, client) {
+  if (err) {
+    console.log (err);
+    return;
+  }
+  
+  console.log ("Successfully connected to MongoDb server.");
+  
+  var db = client.db('muellerfitness');
+  
+  accountsCollection = db.collection ('accounts');
+  classesCollection = db.collection ('classes');
+  
+  console.log ('Successfully created collection objects');
+});
+
+/**
+ * Returns true if the user is admin, false otherwise.
+ * identifier can either be an email address (noted by an '@' symbol) or an ObjectId (from Mongo)
+ */
+function userIsAdmin (identifier) {
+  var queryObject = {};
+  if (identifier.indexOf('@') !== -1) {
+    queryObject['email'] = identifier;
+  } else {
+    queryObject['_id'] = identifier;
+  }
+  
+  var obj = yield accountsCollection.findOne(queryObject, {is_admin: 1});
+  
+  return obj.is_admin;
+}
 
 app.listen(3000, () => console.log('Server listening on port 3000.'));
