248ff8b38cae4f38da082c8f909645ae39356521
Yarden <yardenn1@gmail.com>
2018-04-01 23:06:09 -0400

    Dynamic loading of admin panel classes, css tweaks

--- public/resources/js/admin.js
+++ public/resources/js/admin.js
@@ -4,6 +4,10 @@ app.controller('controller', function ($scope, $http) {
   //Some sample data just for front-end purposes
   $scope.instructor_accounts = [{"name": "Sally", "email": "sally@gmail.com", "classes": ["Yoga", "Pilates"], "bio": "I'm Sally and I'm cool", "image": "/resources/img/i2.jpg"}, {"name": "Bill", "email": "bill@gmail.com", "classes": ["Boxing", "Zumba"], "bio": "I'm Bill and I'm cool", "image": "/resources/img/i3.jpg"}];
   $scope.member_accounts = [{"name": "Yarden Ne'eman", "email": "neemay@rpi.edu", "rin": "660000000", "payment_method": "Bursar", "classes": ["Kettlebell Kickboxing", "Core Yoga"], "payment_status": "1"}, {"name": "Yarden Ne'eman", "email": "neemay@rpi.edu", "rin": "660000000", "payment_method": "Cash", "classes": ["Kettlebell Kickboxing", "Core Yoga"], "payment_status": "0"}];
+  $scope.class_information = [{"name": "Pilates", "instructors": "Deb Something", "start_time": "12:00", "end_time": "13:00", "days": ["Monday", "Thursday"], "description": "Pilates yay!"}, {"name": "Zumba", "instructors": "Sally Sweatsalot", "start_time": "17:00", "end_time": "18:00", "days": ["Monday", "Thursday"], "description": "Zumba yay!"}, {"name": "Zumba 2", "instructors": "Joe Jumpingjacks", "start_time": "13:00", "end_time": "14:00", "days": ["Tuesday"], "description": "Zumba yay x2!"}];
+  $scope.instructor_names = [{"name": "Deb Something"}, {"name": "Joe Jumpingjacks"}, {"name": "Sally Sweatsalot"}];
+  
+  $scope.inst = "Pending";
   $scope.classes = true;
   $scope.payment = "0";
   $scope.method = "Cash";
