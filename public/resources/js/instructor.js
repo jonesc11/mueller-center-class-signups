7598657b7e30172992b8838e5be689c2da8c697b
Sydney Ruzicka <sydneyruzicka@outlook.com>
2018-04-05 11:58:41 -0400

    login page

--- public/resources/js/instructor.js
+++ public/resources/js/instructor.js
@@ -2,6 +2,7 @@ var app = angular.module("instructor-acc", []);
 app.controller('controller', function ($scope, $http) {
   //Testing 
   $scope.account = {"name": "Sally", "email": "sally@gmail.com", "classes": ["Yoga", "Pilates"], "bio": "I'm Sally and I'm cool", "image": "/resources/img/i2.jpg"};
+  $scope.instructor_accounts = [{"name": "Sally", "email": "sally@gmail.com", "classes": ["Yoga", "Pilates"], "bio": "I'm Sally and I'm cool", "image": "/resources/img/i2.jpg"}, {"name": "Bill", "email": "bill@gmail.com", "classes": ["Boxing", "Zumba"], "bio": "I'm Bill and I'm cool", "image": "/resources/img/i3.jpg"}];
   $scope.editorEnabled = false;
   
   $scope.enableEditor = function() {
