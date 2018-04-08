6b404db317089a160fbb13c87f46579cef1d7ceb
Sydney Ruzicka <sydneyruzicka@outlook.com>
2018-04-08 18:57:39 -0400

    password modal

--- public/resources/js/instructor.js
+++ public/resources/js/instructor.js
@@ -1,7 +1,7 @@
 var app = angular.module("instructor-acc", []);
 app.controller('controller', function ($scope, $http) {
   //Testing 
-  $scope.account = {"name": "Sally", "email": "sally@gmail.com", "classes": ["Yoga", "Pilates"], "bio": "I'm Sally and I'm cool", "image": "/resources/img/i2.jpg"};
+  $scope.account = {"name": "Sally","password":"abc123", "email": "sally@gmail.com", "classes": ["Yoga", "Pilates"], "bio": "I'm Sally and I'm cool", "image": "/resources/img/i2.jpg"};
   $scope.editorEnabled = false;
   
   $scope.enableEditor = function() {
@@ -18,6 +18,18 @@ app.controller('controller', function ($scope, $http) {
     $scope.account.name = $scope.editableName;
     $scope.account.bio = $scope.editableBio;
     $scope.disableEditor();
+  };
+
+  $scope.changePass = function() {
+    if ($scope.oldpass != $scope.account.password){
+      alert("Wrong Password! Try Again");
+    }
+    else if (($scope.oldpass == $scope.account.password) && ($scope.newpass != $scope.rtnewpass)){
+      alert("New Password Does Not Match! Try Again");
+    }
+    else if (($scope.oldpass == $scope.account.password) && ($scope.newpass == $scope.rtnewpass)){
+      $scope.account.password = $scope.newpass;
+    }
   };
 
 });
