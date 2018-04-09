3ea3a9e0e9433446f57adca3eae6fb8c8567256d
Sydney Ruzicka <sydneyruzicka@outlook.com>
2018-04-09 18:45:42 -0400

    password modal

--- public/resources/js/instructor.js
+++ public/resources/js/instructor.js
@@ -3,6 +3,9 @@ app.controller('controller', function ($scope, $http) {
   //Testing 
   $scope.account = {"name": "Sally","password":"abc123", "email": "sally@gmail.com", "classes": ["Yoga", "Pilates"], "bio": "I'm Sally and I'm cool", "image": "/resources/img/i2.jpg"};
   $scope.editorEnabled = false;
+  $scope.error1 = false;
+  $scope.error2 = false;
+  $scope.success = false;
   
   $scope.enableEditor = function() {
     $scope.editorEnabled = true;
@@ -21,15 +24,29 @@ app.controller('controller', function ($scope, $http) {
   };
 
   $scope.changePass = function() {
+    document.getElementById("change-pass").reset();
     if ($scope.oldpass != $scope.account.password){
-      alert("Wrong Password! Try Again");
+      $scope.error1 = true;
+      $scope.error2 = false;
+      $scope.success = false;
     }
     else if (($scope.oldpass == $scope.account.password) && ($scope.newpass != $scope.rtnewpass)){
-      alert("New Password Does Not Match! Try Again");
+      $scope.error2 = true;
+      $scope.error1 = false;
+      $scope.success = false;
     }
     else if (($scope.oldpass == $scope.account.password) && ($scope.newpass == $scope.rtnewpass)){
       $scope.account.password = $scope.newpass;
+      $scope.success = true;
+      $scope.error1 = false;
+      $scope.error2 = false;
     }
+  };
+
+  $scope.resetPass = function(){
+    $scope.error1 = false;
+    $scope.error2 = false;
+    $scope.success = false;
   };
 
 });
